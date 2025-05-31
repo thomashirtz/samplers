d49a2fa7696fc50411137a0d3295e45eeb00444f
Thomas Hirtz <hirtzt@gmail.com>
2025-05-31 14:27:50 +0200

    add todos

---
+++ samplers/models/compvis/model.py
@@ -0,0 +1,13 @@
+# the ldm now is usable, I will put all the config in this subfolder with the download things
+# utility to download + save to cache dir (which is mandatory, or see how huggingface does)
+# need to update the packages in latent-diffusion.
+
+
+class FFHQLatentModel:
+    def __init__(self, cache_dir):
+        ...
+        # fetch the config ldmv4
+        # download and unzip
+        # instantiate
+
+    # start only with VQ4, and in the future possibly add more from readme https://github.com/thomashirtz/latent-diffusion/tree/main
