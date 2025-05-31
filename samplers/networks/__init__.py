bff269466ba8837a37846a5cf87072b2c37a965c
Thomas Hirtz <hirtzt@gmail.com>
2025-05-31 18:04:31 +0200

    clean files, start experimenting with psld

--- samplers/networks/__init__.py
+++ samplers/networks/__init__.py
@@ -2,7 +2,6 @@ from .base import LatentNetwork, Network
 from .custom import CustomNetwork
 from .hugging_face.ddpm import DDPMNetwork
 from .hugging_face.stable_diffusion import StableDiffusionNetwork
-from .utilities import build_network
 
 __all__ = [
     "CustomNetwork",
@@ -10,7 +9,6 @@ __all__ = [
     "StableDiffusionNetwork",
     "Network",
     "LatentNetwork",
-    "build_network",
 ]
 
 # fixme actually I don't like that build_network is in __all__, but pycharm is complaining. Also,
